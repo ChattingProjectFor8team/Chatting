a2029e4a80645ba7477d50283335c30faffad431
cherry pick <limhojin956@naver.com>
2026-04-23 22:31:12 +0900

    hofix : parameterstore

--- src/main/java/com/example/infinite/DemoApplication.java
+++ src/main/java/com/example/infinite/DemoApplication.java
@@ -8,7 +8,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @ConfigurationPropertiesScan
 @EnableScheduling
 @SpringBootApplication(exclude = {
-        io.awspring.cloud.autoconfigure.s3.S3AutoConfiguration.class
+        // S3 자동 설정 제외
+        io.awspring.cloud.autoconfigure.s3.S3AutoConfiguration.class,
+        // 파라미터 스토어 및 환경설정 로드 기능 제외
+        io.awspring.cloud.autoconfigure.config.parameterstore.ParameterStoreAutoConfiguration.class
 })
 public class DemoApplication {
 
